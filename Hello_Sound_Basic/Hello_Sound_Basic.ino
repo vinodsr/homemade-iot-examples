7740a3e2e29f288e12ac7fbf6aa85b0f86da67f2
Vinod S R <mailme@vinodsr.com>
2017-03-26 21:10:18 +0530

    Updated

--- Hello_Sound_Basic/Hello_Sound_Basic.ino
+++ Hello_Sound_Basic/Hello_Sound_Basic.ino
@@ -11,6 +11,7 @@ int noteDurations[] = {
   4, 8, 8, 4, 4, 4, 4, 4
 };
 
+const int BUZZ_PIN = 8;
 void setup() {
   // iterate over the notes of the melody:
   for (int thisNote = 0; thisNote < 8; thisNote++) {
@@ -19,14 +20,14 @@ void setup() {
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000 / noteDurations[thisNote];
-    tone(8, melody[thisNote], noteDuration);
+    tone(BUZZ_PIN, melody[thisNote], noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(8);
+    noTone(BUZZ_PIN);
   }
 }
 

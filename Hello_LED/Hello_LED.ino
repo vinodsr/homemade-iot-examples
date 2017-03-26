70a36cea60a0ee4b8b070a505eb0dc66745ce43a
Vinod S R <mailme@vinodsr.com>
2017-03-26 20:49:43 +0530

    Initial Commit

---
+++ Hello_LED/Hello_LED.ino
@@ -0,0 +1,13 @@
+const int LED_PIN = 1;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}

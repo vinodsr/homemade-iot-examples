834c7e6d707630c856127152efca4c8a74798505
Vinod S R <mailme@vinodsr.com>
2017-03-26 21:47:19 +0530

    added IR sensor

---
+++ Hello_IR_Sensor/Hello_IR_Sensor.ino
@@ -0,0 +1,29 @@
+
+
+int LED = 13; // Use the onboard Uno LED
+int isObstaclePin = A0;  // This is our input pin
+int isObstacle = HIGH;  // HIGH MEANS NO OBSTACLE
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(isObstaclePin, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  isObstacle = analogRead(isObstaclePin);
+  Serial.println(isObstacle);
+  if (isObstacle == LOW)
+  {
+    Serial.println("OBSTACLE!!, OBSTACLE!!");
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    Serial.println("clear");
+    digitalWrite(LED, LOW);
+  }
+  delay(200);
+}
+
